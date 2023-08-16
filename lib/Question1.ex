2833400809ccfcb5d4de9a31bd118f69fd1a5217
magashpillay <magashpillay@gmail.com>
2023-08-16 12:35:32 +0200

    all fixed and tests passing

--- lib/Question1.ex
+++ lib/Question1.ex
@@ -22,16 +22,16 @@ defmodule Question1 do
   @spec hello({:ok, binary}) :: <<_::48, _::_*8>>
   @doc """
 
-  # Q1.1 -(10) Please write doc test
+  # Q1.1 -(10) Please write doc
 
   ## Examples
       iex> Question1.hello({:ok,"mags"})
       "mags hello"
   """
-  def hello{:ok, b} do
-    case  is_bitstring(b) do
-      :true -> b <> " hello"
-      :false -> {:error}
-    end
+  def hello {:ok, b}  do
+     case  is_bitstring(b) do
+       :true -> b <> " hello"
+       :false -> {:error}
+     end
   end
 end
